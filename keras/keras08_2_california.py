00b2ff6039785ffd54f64578ec5c7352d6f59772
Leejoonkwon <qnfk7895@naver.com>
2022-06-23 13:15:58 +0900

    "first commit

---
+++ keras/keras08_2_california.py
@@ -0,0 +1,52 @@
+from sklearn.datasets import fetch_california_housing
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+
+#1. 데이터
+datasets = fetch_california_housing()
+x = datasets.data
+y = datasets.target
+x_train, x_test ,y_train, y_test = train_test_split(
+          x, y, train_size=0.9,shuffle=True,random_state=12)
+print(x)
+print(y)
+print(x.shape) # (20640, 8)
+print(y.shape) # (20640,)
+
+print(datasets.feature_names)
+print(datasets.DESCR)
+
+#2. 모델구성
+model = Sequential()
+model.add(Dense(8, input_dim=8))
+model.add(Dense(32))
+model.add(Dense(34))
+model.add(Dense(38))
+model.add(Dense(42))
+model.add(Dense(50))
+model.add(Dense(32))
+model.add(Dense(24))
+model.add(Dense(12))
+model.add(Dense(8))
+model.add(Dense(1))
+
+#3. 컴파일,훈련
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, epochs=50, batch_size=82)
+
+#4. 평가,예측
+loss = model.evaluate(x_test, y_test)
+print('loss :', loss)
+
+y_predict = model.predict(x_test)
+
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+print('r2스코어 :', r2)
+
+# [실습 시작!!] #0.57이상
+# loss : 0.637947678565979
+# r2스코어 : 0.5350813419599821
+
