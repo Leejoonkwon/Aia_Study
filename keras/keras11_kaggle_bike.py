634b18ce8c57b12dd4961031496172d293307a6d
Leejoonkwon <qnfk7895@naver.com>
2022-06-28 19:56:57 +0900

    ' 저장

--- keras/keras11_kaggle_bike.py
+++ keras/keras11_kaggle_bike.py
@@ -1,113 +1,87 @@
-# 캐글 자전거 문제풀이
+# 데이콘 따릉이 문제풀이
 import numpy as np
 import pandas as pd
-from sqlalchemy import true #pandas : 엑셀땡겨올때 씀
+from sqlalchemy import null
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.ensemble import RandomForestRegressor
-import datetime as datetime
 
 #1. 데이터
-path = './_data/kaggle_bike/'
-train_set = pd.read_csv(path + 'train.csv') # + 명령어는 문자를 앞문자와 더해줌  index_col=n n번째 컬럼을 인덱스로 인식
+path = './_data/kaggle_bike/' # ".은 현재 폴더"
+train_set = pd.read_csv(path + 'train.csv',
+                        index_col=0)
+print(train_set)
+
+print(train_set.shape) #(10886, 11)
+
+test_set = pd.read_csv(path + 'test.csv', #예측에서 쓸거야!!
+                       index_col=0)
+
+sampleSubmission = pd.read_csv(path + 'sampleSubmission.csv',#예측에서 쓸거야!!
+                       index_col=0)
             
-test_set = pd.read_csv(path + 'test.csv') # 예측에서 쓸거임        
-
-'''                        
-print(train_set)
-print(train_set.shape) # (10886, 12)
-                  
 print(test_set)
-print(test_set.shape) # (6493, 9)
-print(test_set.info()) # (715, 9)
+print(test_set.shape) #(6493, 8) #train_set과 열 값이 '1'차이 나는 건 count를 제외했기 때문이다.예측 단계에서 값을 대입
+
 print(train_set.columns)
-print(train_set.info()) # info 정보출력
-print(train_set.describe()) # describe 평균치, 중간값, 최소값 등등 출력
-'''
+print(train_set.info()) #null은 누락된 값이라고 하고 "결측치"라고도 한다.
+print(train_set.describe()) 
 
 
-######## 년, 월 ,일 ,시간 분리 ############
-
-train_set["hour"] = [t.hour for t in pd.DatetimeIndex(train_set.datetime)]
-train_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(train_set.datetime)]
-train_set["month"] = [t.month for t in pd.DatetimeIndex(train_set.datetime)]
-train_set['year'] = [t.year for t in pd.DatetimeIndex(train_set.datetime)]
-train_set['year'] = train_set['year'].map({2011:0, 2012:1})
-
-test_set["hour"] = [t.hour for t in pd.DatetimeIndex(test_set.datetime)]
-test_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(test_set.datetime)]
-test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
-test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
-test_set['year'] = test_set['year'].map({2011:0, 2012:1})
-
-test_set = train_set.drop('datetime',axis=1) # 트레인 세트에서 데이트타임 드랍
-test_set = train_set.drop('casual',axis=1) # 트레인 세트에서 캐주얼 레지스터드 드랍
-test_set = train_set.drop('registered',axis=1)
-
-test_set.drop('datetime',axis=1) # 트레인 세트에서 데이트타임 드랍
-
-print(train_set)
-print(test_set)
-##########################################
+###### 결측치 처리 1.제거##### dropna 사용
+print(train_set.isnull().sum()) #각 컬럼당 결측치의 합계
+print(train_set.shape) #(10886,11)
 
 
-x = train_set.drop(['count'], axis=1)  # drop 데이터에서 ''사이 값 빼기
-print(x)
+x = train_set.drop([ 'casual', 'registered','count'],axis=1) #axis는 컬럼 
+
+
 print(x.columns)
-print(x.shape) # (10886, 12)
+print(x.shape) #(10886, 8)
 
-y = train_set['count'] 
-print(y)
-print(y.shape) # (10886,)
-'''
-x_train, x_test, y_train, y_test = train_test_split(x,y,
-                                                    train_size=0.75,
-                                                    random_state=31
-                                                    )
+y = train_set['count']
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size = 0.949, shuffle = True, random_state = 100
+ )
+print(test_set)
+# print(y)
+# print(y.shape) # (10886,)
+
 
 #2. 모델구성
+
 model = Sequential()
-model.add(Dense(100, activation='elu', input_dim=12))
+model.add(Dense(100,input_dim=8))
 model.add(Dense(100, activation='swish'))
-model.add(Dense(100, activation='elu'))
 model.add(Dense(100, activation='swish'))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=800, batch_size=100, verbose=1)
+model.compile(loss='mae', optimizer='adam')
+model.fit(x, y , epochs =3936, batch_size=178, verbose=2)
 
 #4. 평가, 예측
-loss = model.evaluate(x, y) 
-print('loss : ', loss)
+loss = model.evaluate(x_test, y_test)
+print('loss :',loss)
 
-y_predict = model.predict(x_test)
+y_predict = model.predict(x_test) #훈련으로 예측된 값 y_predict와 원래 테스트 값 y_test와 비교
 
-def RMSE(a, b): 
-    return np.sqrt(mean_squared_error(a, b))
-
+def RMSE(y_test, y_predict):
+     return np.sqrt(mean_squared_error(y_test, y_predict))
 rmse = RMSE(y_test, y_predict)
-print("RMSE : ", rmse)
-
-# loss :  708.68310546875
-# RMSE :  42.18903164279918
+print("RMSE :",rmse)  
 
 y_summit = model.predict(test_set)
+# print(y_summit)
+# print(y_summit.shape)
 
-print(y_summit)
-print(y_summit.shape) # (6493, 1)
-
-submission_set = pd.read_csv(path + 'sampleSubmission.csv', # + 명령어는 문자를 앞문자와 더해줌
-                             index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
-
-print(submission_set)
-
-submission_set['count'] = y_summit
-print(submission_set)
+# loss : 55.3439826965332
+# RMSE : 98.98607995677362
+# train_size = 0.935, shuffle = True, random_state = 100
+# epochs =5830, batch_size=516, verbose=2
+sampleSubmission['count'] = y_summit
+sampleSubmission.to_csv('test16.csv',index=True)
 
 
-submission_set.to_csv(path + 'sampleubmission.csv', index = True)
 
-'''
