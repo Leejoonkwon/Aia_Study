5fcca011a39d6305412009ba23ca980b7420837a
Leejoonkwon <qnfk7895@naver.com>
2022-07-17 21:15:07 +0900

    0717

--- keras/keras50_kaggle(test).py
+++ keras/keras50_kaggle(test).py
@@ -34,35 +34,48 @@ plt.plot(data['T (degC)'][0:6*24*365],color="black",linewidth=0.2)
 # plt.show()
 
 data.index = pd.to_datetime(data['Date Time'],
-                            format = "%d.%m.%Y %H:%M:%S") # europeean format
+                            format = "%d.%m.%Y %H:%M:%S")
+# data에 index 열을 Date Time에 연,월,일,시간,분,초를 각각 문자열로 인식해 대체합니다.
 hourly = data[5::6]
+# 5번째 인수의 데이터를 시작으로 6번째에 위치한 데이터만 반환합니다. 
+# [5::6]으로 자른 이유는 데이터를 시간단위로 분할하기 위해서다.
+#예) 데이터가 [1,2,3,4,5,6,7,8,9,10]을  [1:3]으로 자른다면 [2,5,8]이 된다.
 hourly=hourly.drop_duplicates()
+#[70067 rows x 15 columns] 중복되는 값은 제거한다 행이 70091->에서 70067로 줄어든 것을 확인
 hourly.duplicated().sum()
+# 0 중복된 값이 없는 것을 확인
 daily = data['T (degC)'].resample('1D').mean().interpolate('linear')
 #resample은 본인이 가진 데이터 중 원하는 값만 뽑아냄 시계열 데이터에서 자주 활용 '1D'는 단위 구간을 1일로 설정
 daily[0:365].plot()
 # plt.show() # 월별 온도 확인
 hourly_temp = hourly['T (degC)']
-len(hourly_temp)
+len(hourly_temp) #70067
+
 def generator(data, window, offset):
-    gen = data.to_numpy()
-    X = []
+    gen = data.to_numpy() #데이터 프레임을 배열객체로 반환
+    x = []
     y = []
-    for i in range(len(gen)-window-offset):
+    for i in range(len(gen)-window-offset): #range("")는 ("")값만큼 반복하겠다는 의미
         row = [[a] for a in gen[i:i+window]]
-        X.append(row)
+        x.append(row)
         label = gen[i+window+offset-1]
         y.append(label)
-    return np.array(X), np.array(y)
+    return np.array(x), np.array(y)
 WINDOW = 5
 OFFSET = 24
-X, y = generator(hourly_temp, WINDOW, OFFSET)
-X_train, y_train = X[:60000], y[:60000]
-X_val, y_val = X[60000:65000], y[60000:65000]
-X_test, y_test = X[65000:], y[65000:]
-print(X_train,X_test) #(49026, 5, 1) (21012, 5, 1)
+x, y = generator(hourly_temp, WINDOW, OFFSET)
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,shuffle=False,train_size=0.85)
+# X_train, y_train = X[:60000], y[:60000]
+# X_val, y_val = X[60000:65000], y[60000:65000]
+# X_test, y_test = X[65000:], y[65000:]
+print(x_train.shape,x_test.shape) #(49026, 5, 1) (21012, 5, 1)
+print(y_train.shape,y_test.shape) #(49026, 5, 1) (21012, 5, 1)
+'''
+
 #시계열 데이터의 특성 상 연속성을 위해서 train_test_split에 셔플을 배제하기 위해
 #위 명령어로 정의한다.suffle을 False로 놓고 해도 될지는 모르겠다.
+
 from tensorflow.python.keras.models import Sequential,Model
 from tensorflow.python.keras.layers import LSTM,Conv1D,Flatten,Reshape
 from tensorflow.python.keras.layers import InputLayer,Input,Dense
@@ -80,12 +93,12 @@ output1 = Dense(1,activation='relu',name='out_jk1')(dense4)
 model = Model(inputs=input1, outputs=output1)
 model.summary()
 from tensorflow.python.keras.callbacks import EarlyStopping,ModelCheckpoint
-earlyStopping = EarlyStopping(monitor='loss', patience=10, mode='min', 
+earlyStopping = EarlyStopping(monitor='loss', patience=100, mode='min', 
                               verbose=1,restore_best_weights=True)
 #3. 컴파일,훈련
 model.compile(loss='mae', optimizer='Adam')
 model.fit(X_train, y_train, validation_data=(X_val, y_val),
-          epochs=100,batch_size=4096,
+          epochs=1000,batch_size=4096,
           callbacks=[earlyStopping]
           ,verbose=2)
 
@@ -93,7 +106,7 @@ model.fit(X_train, y_train, validation_data=(X_val, y_val),
 loss = model.evaluate(X_test, y_test)
 print("loss :",loss)
 # loss : 2.3783624172210693
-'''
+
 test_predictions = model1.predict(X_test).flatten()
 
 result = pd.DataFrame(data={'Predicted': test_predictions, 'Real':y_test})
