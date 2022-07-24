f02877335fd9ea84d3755ac954c4df4c68cd98b2
Leejoonkwon <qnfk7895@naver.com>
2022-07-24 22:09:14 +0900

    0724자

--- keras/keras53_project.py
+++ keras/keras53_project.py
@@ -21,48 +21,54 @@ from sklearn import datasets
 
 #1. 데이터
 
-train = ImageDataGenerator(
-    rescale=1./255,               # MinMax 스케일링과 같은 개념 
-  )                               # 회전 축소 등으로 이미지에 여백이생겼을때 채우는 방법입니다.
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    horizontal_flip=True,
+    # vertical_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    rotation_range=5,
+    zoom_range=0.1,
+    # shear_range=0.7,
+    fill_mode='nearest')
+                 
 test_datagen = ImageDataGenerator(
     rescale=1./255)
 
-xydata = train.flow_from_directory(
-    'C:\study\Study\_data\emotion\\train',
+xy_train= train_datagen.flow_from_directory(
+    'C:\_data\emotion\\train',
     target_size=(150,150),
     class_mode='categorical',
-    batch_size=500,
+    batch_size=28709,
     shuffle=True,) # 경로 및 폴더 설정
-
+xy_test= test_datagen.flow_from_directory(
+    'C:\_data\emotion\\test',
+    target_size=(150,150),
+    class_mode='categorical',
+    batch_size=7178,
+    shuffle=True,) # 경로 및 폴더 설정
 # print(xydata[0][0],xydata[0][0].shape) # (500, 150, 150, 3)
-print(xydata[0][0].shape) # (500, 150, 150, 3)
-print(xydata[0][1].shape) # (500,)
+print(xy_train[0][0].shape) # (500, 150, 150, 3)
+print(xy_train[0][1].shape) # (500, 7)
+x_train = xy_train[0][0]
+y_train = xy_train[0][1]
+x_test = xy_test[0][0]
+y_test = xy_test[0][1]
 
-x_train = xydata[0][0][0:450]
-y_train = xydata[0][1][0:450]
-x_test = xydata[0][0][450:]
-y_test = xydata[0][1][450:]
-
-print(x_train.shape) # (450, 150, 150, 3)
-print(y_train.shape) # (450,3)
-
-
-augument_size = 500
+augument_size = 5000
 randidx = np.random.randint(x_train.shape[0],size=augument_size)
 
 x_augumented = x_train[randidx].copy()
 y_augumented = y_train[randidx].copy()
 
 
-print(x_augumented.shape)  #(400, 28, 28)
-print(y_augumented.shape) #(400,) 50000, 32, 32, 3)
-print(x_train.shape) #(160, 150, 150, 1)
+print(x_augumented.shape)  #(10000, 150, 150, 3)
+print(y_augumented.shape) #(10000, 7)
+print(x_train.shape) #(28709, 150, 150, 3)
 
 
 
 
-xy_df2 = train.flow(x_train,y_train,
-                                  batch_size=augument_size,shuffle=False)
 x_df = np.concatenate((x_train,x_augumented))
 y_df = np.concatenate((y_train,y_augumented))
 # print(x_df.shape) #(64000, 28, 28, 1)
@@ -70,10 +76,10 @@ y_df = np.concatenate((y_train,y_augumented))
 xy_df3 = test_datagen.flow(x_df,y_df,
                        batch_size=augument_size,shuffle=False)
 
-np.save('D:/study_data/_save/_npy/keras49_9_train_x.npy',arr=xy_df3[0][0])
-np.save('D:/study_data/_save/_npy/keras49_9_train_y.npy',arr=xy_df3[0][1])
-np.save('D:/study_data/_save/_npy/keras49_9_test_x.npy',arr=x_test)
-np.save('D:/study_data/_save/_npy/keras49_9_test_y.npy',arr=y_test)
+np.save('C:\study\Study\_save\_train_x.npy',arr=xy_df3[0][0])
+np.save('C:\study\Study\_save\_train_y.npy',arr=xy_df3[0][1])
+np.save('C:\study\Study\_save\_test_x.npy',arr=x_test)
+np.save('C:\study\Study\_save\_test_y.npy',arr=y_test)
 
 
 
---
+++ keras/keras53_project2.py
@@ -0,0 +1,56 @@
+import numpy as np      
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+# np.save('C:\study\Study\_save_train_x.npy',arr=xy_df3[0][0])
+# np.save('C:\study\Study\_save_train_y.npy',arr=xy_df3[0][1])
+# np.save('C:\study\Study\_save_test_x.npy',arr=x_test)
+# np.save('C:\study\Study\_save_test_y.npy',arr=y_test)
+x_train = np.load('C:\study\Study\_save\_save_train_x.npy')
+y_train = np.load('C:\study\Study\_save\_save_train_y.npy')
+x_test = np.load('C:\study\Study\_save\_save_test_x.npy')
+y_test = np.load('C:\study\Study\_save\_save_test_y.npy')
+print(x_train.shape,y_train.shape)  # (10000, 150, 150, 3) (10000, 7)
+print(x_test.shape,y_test.shape)    # (7178, 150, 150, 3) (7178, 7)
+
+
+#2. 모델 
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Conv2D,Flatten,Dense,MaxPool2D
+
+model = Sequential()
+model.add(Conv2D(32,(2,2),input_shape=(150,150,3),padding='same',activation='relu'))
+model.add(MaxPool2D())
+model.add(Conv2D(64,(3,3),activation='relu'))
+model.add(Flatten())
+model.add(Dense(100,activation='relu'))
+model.add(Dense(100,activation='relu'))
+model.add(Dense(7,activation='softmax'))
+import time
+start_time = time.time()
+#3. 컴파일,훈련
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+hist = model.fit(x_train,y_train,epochs=2,verbose=2,validation_split=0.25,batch_size=500)
+
+
+#4. 평가,예측
+loss = model.evaluate(x_test, y_test)
+print('loss :', loss)
+end_time = time.time()-start_time
+print("걸린 시간 :",end_time)
+# y_predict = model.predict(x_test)
+# y_predict = np.argmax(y_predict,axis=1)
+# y_test = np.argmax(y_test,axis=1)
+
+# print('y_predict :', y_predict.shape) #y_predict : (50,)
+# from sklearn.metrics import accuracy_score
+# acc = accuracy_score(y_test, y_predict)
+# print('acc 스코어 :', acc)
+# loss : 1.9426530599594116
+# val_loss : 0.901305615901947
+
+#### 증폭 후
+# loss : [539.5560302734375, 0.23999999463558197]
+# y_predict : (50,)
+# acc 스코어 : 0.24
