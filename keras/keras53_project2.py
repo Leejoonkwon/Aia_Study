ab876df32395c5594fca7aa6f97e750e72784862
Leejoonkwon <107663853+Leejoonkwon@users.noreply.github.com>
2022-07-25 20:58:33 +0900

    0725자

--- keras/keras53_project2.py
+++ keras/keras53_project2.py
@@ -20,19 +20,21 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D,Flatten,Dense,MaxPool2D
 
 model = Sequential()
-model.add(Conv2D(32,(2,2),input_shape=(150,150,3),padding='same',activation='relu'))
+model.add(Conv2D(64,(2,2),input_shape=(150,150,3),padding='same',activation='relu'))
 model.add(MaxPool2D())
-model.add(Conv2D(16,(3,3),activation='relu'))
+model.add(Conv2D(32,(3,3),activation='relu'))
 model.add(Flatten())
-model.add(Dense(10,activation='relu'))
-model.add(Dense(10,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(32,activation='relu'))
 model.add(Dense(7,activation='softmax'))
 import time
 start_time = time.time()
 #3. 컴파일,훈련
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-hist = model.fit(x_train,y_train,epochs=10,verbose=2,validation_split=0.25,batch_size=100)
-
+hist = model.fit(x_train,y_train,epochs=250,verbose=2,validation_split=0.25,batch_size=150)
+# model.save_weights
 
 #4. 평가,예측
 loss = model.evaluate(x_test, y_test)
@@ -55,6 +57,11 @@ print('acc 스코어 :', acc)
 # y_predict : (50,)
 # acc 스코어 : 0.24
 # 걸린 시간 : 6.950930118560791
+
+# loss : [1913.9832763671875, 0.20813597738742828]
+# 걸린 시간 : 72.83188486099243
+# y_predict : (7178,)
+# acc 스코어 : 0.20813597102256895
 
 
 
