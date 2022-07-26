dee79eefbd53b7a82e3816acf51e4dd414f389aa
Leejoonkwon <qnfk7895@naver.com>
2022-07-26 20:49:44 +0900

    0726자

--- keras/keras53_project2.py
+++ keras/keras53_project2.py
@@ -11,8 +11,8 @@ x_train = np.load('D:\study_data\_save\_npy\_train_x1.npy')
 y_train = np.load('D:\study_data\_save\_npy\_train_y1.npy')
 x_test = np.load('D:\study_data\_save\_npy\_test_x1.npy')
 y_test = np.load('D:\study_data\_save\_npy\_test_y1.npy')
-print(x_train.shape,y_train.shape)  # (15000, 150, 150, 1) (15000, 21)
-print(x_test.shape,y_test.shape)    # (5000, 150, 150, 1) (5000, 21)
+# print(x_train.shape,y_train.shape)  # (15000, 150, 150, 1) (15000, 21)
+# print(x_test.shape,y_test.shape)    # (5000, 150, 150, 1) (5000, 21)
 
 
 #2. 모델 
@@ -20,13 +20,13 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D,Flatten,Dense,MaxPool2D,Dropout
 
 model = Sequential()
-model.add(Conv2D(64,(2,2),input_shape=(150,150,1),padding='same',activation='relu'))
+model.add(Conv2D(64,(3,3),input_shape=(150,150,1),padding='same',activation='relu'))
 model.add(MaxPool2D())
-model.add(Conv2D(32,(3,3),activation='relu'))
+model.add(Conv2D(64,(3,3),activation='relu'))
 model.add(Flatten())
-model.add(Dense(32,activation='relu'))
+model.add(Dense(64,activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(32,activation='relu'))
+model.add(Dense(64,activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(32,activation='relu'))
 model.add(Dropout(0.2))
@@ -38,8 +38,8 @@ from keras.callbacks import ModelCheckpoint,EarlyStopping
 
 start_time = time.time()
 #3. 컴파일,훈련
-filepath = './_test/'
-filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+# filepath = './_test/'
+# filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
 earlyStopping = EarlyStopping(monitor='loss', patience=30, mode='min', 
                               verbose=1,restore_best_weights=True)
 # mcp = ModelCheckpoint(monitor='val_loss',mode='auto',verbose=1,
@@ -48,10 +48,10 @@ earlyStopping = EarlyStopping(monitor='loss', patience=30, mode='min',
 #                     )
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 hist = model.fit(x_train,y_train,epochs=350,verbose=2,
-                 validation_split=0.25,batch_size=500
+                 validation_split=0.25,batch_size=150
                  ,callbacks=[earlyStopping])
-model.save_weights("D:\study_data\_save.h5")
-model.save_weights("./_save/keras23_5_save_weights1.h5")
+model.save_weights("D:\study_data\_save\keras53_project2.h5")
+# model.save_weights("./_save/keras23_5_save_weights1.h5")
 #4. 평가,예측
 loss = model.evaluate(x_test, y_test)
 print('loss :', loss)
@@ -68,7 +68,10 @@ print('acc 스코어 :', acc)
 #### 증폭 후
 #acc 스코어 : 0.1812
 
-
+# loss : [8.893112182617188, 0.2540000081062317]
+# 걸린 시간 : 1010.0424473285675
+# y_predict : (5000,)
+# acc 스코어 : 0.254
 
 
 
