844db22551e5f76e7d1cf8f0110291fba48cd89f
Leejoonkwon <qnfk7895@naver.com>
2022-07-28 10:34:50 +0900

    0728자

--- keras/keras53_project3(size_100,100).py
+++ keras/keras53_project3(size_100,100).py
@@ -12,8 +12,8 @@ x_train = np.load('D:\study_data\_save\_npy\_train_x5.npy')
 y_train = np.load('D:\study_data\_save\_npy\_train_y5.npy')
 x_test = np.load('D:\study_data\_save\_npy\_test_x5.npy')
 y_test = np.load('D:\study_data\_save\_npy\_test_y5.npy')
-print(x_train.shape,y_train.shape)  # (27210, 48, 48, 1) (27210, 21)
-print(x_test.shape,y_test.shape)    # (11662, 48, 48, 1) (11662, 21)
+# print(x_train.shape,y_train.shape)  # (27097, 100, 100, 1) (27097, 21)
+# print(x_test.shape,y_test.shape) # (6775, 100, 100, 1) (6775, 21)
 
 # import matplotlib.pyplot as plt
 # plt.figure(figsize=(100,5))
@@ -28,7 +28,7 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D,Flatten,Dense,MaxPool2D,Dropout
 
 model = Sequential()
-model.add(Conv2D(48,(2,2),input_shape=(48,48,1),padding='same',activation='relu'))
+model.add(Conv2D(48,(2,2),input_shape=(100,100,1),padding='same',activation='relu'))
 model.add(MaxPool2D((2,2)))
 model.add(Conv2D(64,(2,2),padding='same',activation='relu'))
 model.add(MaxPool2D((2,2)))
@@ -52,9 +52,9 @@ earlyStopping = EarlyStopping(monitor='val_loss', patience=10, mode='min',
 #                       filepath="".join([filepath,'k24_', date, '_', filename])
 #                     )
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-hist = model.fit(x_train,y_train,epochs=30,verbose=2,
+hist = model.fit(x_train,y_train,epochs=500,verbose=2,
                  validation_split=0.3,
-                 batch_size=2000,
+                 batch_size=100,
                  callbacks=[earlyStopping])
 model.save_weights("D:\study_data\_save\keras53_project2.h5")
 # model.save_weights("./_save/keras23_5_save_weights1.h5")
@@ -73,18 +73,10 @@ acc = accuracy_score(y_test, y_predict)
 print('acc 스코어 :', acc)
 
 #### 증폭 후
-#acc 스코어 : 0.1812
-
-# loss : [8.893112182617188, 0.2540000081062317]
-# 걸린 시간 : 1010.0424473285675
-# y_predict : (5000,)
-# acc 스코어 : 0.254
-
-# loss : [1.862046718597412, 0.24500000476837158]
-# 걸린 시간 : 35.0883367061615
-# y_predict : (5000,)
-# acc 스코어 : 0.245
-
+# loss : [1.5243678092956543, 0.39542436599731445]
+# 걸린 시간 : 47.57860064506531
+# y_predict : (6775,)
+# acc 스코어 : 0.39542435424354244
 
 
 
