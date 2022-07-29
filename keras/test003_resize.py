06495f3d9a182553df66ef42dfd3829927e28b5b
Leejoonkwon <qnfk7895@naver.com>
2022-07-29 14:49:56 +0900

    0729자

--- keras/test003_resize.py
+++ keras/test003_resize.py
@@ -1,50 +1,12 @@
-from tensorflow.keras.datasets import fashion_mnist
-from keras.preprocessing.image import ImageDataGenerator 
+import cv2
+import os
 import numpy as np
-
-
-
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True,
-    # vertical_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    rotation_range=5,
-    zoom_range=0.1,
-    # shear_range=0.7,
-    fill_mode='nearest'
-)
-test_datagen = ImageDataGenerator(
-    rescale=1./255,
-   
-)
-xy_train= train_datagen.flow_from_directory(
-    'D:\study_data\_data\emotion\\train',
-    target_size=(150,150),
-    class_mode='categorical',
-    batch_size=28709,
-    shuffle=True,) # 경로 및 폴더 설정
-x_train = xy_train[0][0]
-
-augument_size = 10
-randidx = np.random.randint(x_train.shape[0],size=augument_size)
-
-# print(randidx,randidx.shape) #((20,)
-
-# print(np.min(randidx),np.max(randidx)) # 174 49920
-# print(type(randidx)) #<class 'numpy.ndarray'>
-
-x_augumented = x_train[randidx].copy()
-x_train = x_train[randidx].copy()
-
-
-# 1. x_augumented 10개와 x_train 10개를 비교하는 이미지 출력할 것
-import matplotlib.pyplot as plt
-plt.figure(figsize=(7,7))
-for i in range(20):
-    plt.subplot(2,10,i+1)
-    plt.axis('off')
-    plt.imshow(xy_train[0][i],cmap='gray')
-    
-plt.show() 
+from PIL import Image
+ 
+path = 'D:\study_data\\train\\doubt2'
+imagePaths = [os.path.join(path,file_name) for file_name in os.listdir(path)]
+for imagePath in imagePaths:
+    img = Image.open(imagePath).convert('L')
+    img_numpy = np.array(img, 'uint8')
+    cv2.imwrite("D:\study_data\\train\\doubt2\\" + imagePath.split("\\")[-1], img_numpy)
+print("All Done")
