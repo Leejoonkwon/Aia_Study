d4cc3ab488f8f075c09626c3842486ca5c912852
Leejoonkwon <qnfk7895@naver.com>
2022-08-01 09:59:50 +0900

    0801

--- keras/keras53_project(token).py
+++ keras/keras53_project(token).py
@@ -20,20 +20,14 @@ print(df.describe) #[400 rows x 4 columns]
 token = Tokenizer(oov_token="<OOV>") #oov = out of vocabulary 
 df_1 = df['lyric']
 
-# token.fit_on_texts(df_1)
-# x = token.texts_to_sequences(df_1)
-print(sent_tokenize(df_1))
-''''
-le = LabelEncoder()
-df['Genre'] = le.fit_transform(df['Genre'])
-# print(df['Genre'])
-df_1 = df['lyric']
-df_2 = df['title']
-df_3 = df['artist']
-y_data = df['Genre']
 token.fit_on_texts(df_1)
 x = token.texts_to_sequences(df_1)
-print(x)
+print(token.word_index)
+'''
+le = LabelEncoder()
+df['Genre'] = le.fit_transform(df['Genre'])
+y = df['Genre']
+
 print(token.word_index)
 word_size = len(token.word_index)
 print("wored_size :",word_size) #단어 사전의 갯수 : 15237
@@ -41,17 +35,6 @@ from keras.preprocessing.sequence import pad_sequences
 pad_x =pad_sequences(x,padding='pre',maxlen=5)
 print(np.unique(pad_x,return_counts=True))
 print(pad_x.shape)
-
-
-y = token.texts_to_sequences(df_2)
-z = token.texts_to_sequences(df_3)
-x= np.array(x)
-y= np.array(y)
-z= np.array(z)
-print(x.shape,y.shape,z.shape) #(400,) (400,) (400,)
-data = list(map(list.__add__,x,y))
-x_data = list(map(list.__add__,data,z))
-x_data = np.array(x_data)
 
 
 x_train,x_test,y_train,y_test= train_test_split(x_data,y_data,train_size=0.8,shuffle=True,random_state=100)
