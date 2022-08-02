5662c08e26fda3e65a3b0618db28426896740986
Leejoonkwon <qnfk7895@naver.com>
2022-08-02 17:58:32 +0900

    0802

--- keras/keras53_project3(size_100,100,VGG16)2.py
+++ keras/keras53_project3(size_100,100,VGG16)2.py
@@ -1,4 +1,5 @@
 from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
 from keras.preprocessing.image import ImageDataGenerator 
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten
 from tensorflow.python.keras.layers import Dense, Dropout, Input
@@ -7,12 +8,12 @@ import numpy as np
 from keras import models, layers
 from keras import Input
 from keras.models import Model
-
 x_train = np.load('D:\study_data\_save\_npy\_train_x10.npy')
 y_train = np.load('D:\study_data\_save\_npy\_train_y10.npy')
 x_test = np.load('D:\study_data\_save\_npy\_test_x10.npy')
 y_test = np.load('D:\study_data\_save\_npy\_test_y10.npy')
-
+print(x_train.shape) #
+'''
 # 모델 Layer 데이터화
 # input_tensor = Input(shape=(100,100,3))
 pre_trained_vgg = VGG16(weights='imagenet', include_top=False, input_shape=(100, 100, 3))
@@ -38,3 +39,4 @@ hist = additional_model.fit(x_train,y_train,epochs=10,verbose=2,
 #4. 평가,예측
 loss = additional_model.evaluate(x_test, y_test)
 print('loss :', loss)
+'''
--- keras/keras53_project4(augument).py
+++ keras/keras53_project4(augument).py
@@ -11,38 +11,30 @@ train_datagen = ImageDataGenerator(
     fill_mode='nearest')
 test_datagen = ImageDataGenerator(
     rescale=1./255,)
-target_size = 100
+target_size = 48
 xy_train= train_datagen.flow_from_directory(
     'D:\study_data\project\\train',
     target_size=(target_size,target_size),
     class_mode='categorical',
-        batch_size=15000,
+    color_mode='grayscale',
+        batch_size=28709,
     shuffle=True,) # 28709
 xy_test = test_datagen.flow_from_directory(
     'D:\study_data\project\\test',
     target_size=(target_size,target_size),
     class_mode='categorical',
+    color_mode='grayscale',
         batch_size=10000,
     shuffle=False,) # 7178
 # np.save('D:\study_data\_save\_npy\_train_x17.npy',arr=xy_test[0][0])
-x_data1 = np.concatenate((xy_train[0][0],xy_test[0][0])) 
-y_data1 = np.concatenate((xy_train[0][1],xy_test[0][1]))
 
-x_train,x_test,y_train,y_test = train_test_split(x_data1,
-                                                 y_data1,train_size=0.8,shuffle=True,random_state=100)
-# augument_size = 40000
-# randidx = np.random.randint(x_train.shape[0],size=augument_size)
-# x_augumented = x_train[randidx].copy()
-# y_augumented = y_train[randidx].copy()
-# x_data = train_datagen.flow(x_augumented,y_augumented,
-#     batch_size=augument_size,
-#     shuffle=False)
-# x_data1 = np.concatenate((x_train,x_data[0][0])) 
-# y_data1 = np.concatenate((y_train,y_augumented))
-np.save('D:\study_data\_save\_npy\_train_x10.npy',arr=x_train)
-np.save('D:\study_data\_save\_npy\_train_y10.npy',arr=y_train)
-np.save('D:\study_data\_save\_npy\_test_x10.npy',arr=x_test)
-np.save('D:\study_data\_save\_npy\_test_y10.npy',arr=y_test)
+
+
+
+np.save('D:\study_data\_save\_npy\_train_x10.npy',arr=xy_train[0][0])
+np.save('D:\study_data\_save\_npy\_train_y10.npy',arr=xy_train[0][1])
+np.save('D:\study_data\_save\_npy\_test_x10.npy',arr=xy_test[0][0])
+np.save('D:\study_data\_save\_npy\_test_y10.npy',arr=xy_test[0][1])
 
 
 
