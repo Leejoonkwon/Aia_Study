a386422282645dda52885faf44fd0705fe448fc3
Leejoonkwon <107663853+Leejoonkwon@users.noreply.github.com>
2022-08-02 17:41:07 +0900

    0802

--- keras/keras55_ly3.py
+++ keras/keras55_ly3.py
@@ -7,7 +7,7 @@ import re
 import pandas as pd
 
 # title=driver.find_elements_by_class_name
-driver.get("https://www.melon.com/chart/day/index.htm?classCd=GN0800")
+driver.get("https://www.melon.com/chart/day/index.htm?classCd=GN0500")
 title=driver.find_elements(By.CLASS_NAME,'ellipsis.rank01')
 
 title2=[]
@@ -26,7 +26,7 @@ for i in singer:
 del singer2[0]
 del singer2[50:]
 
-songTagList = driver.find_elements(By.ID,'lst100')
+songTagList = driver.find_elements(By.ID,'lst50')
 number=[]
 for i in songTagList:
     number.append(i.get_attribute('data-song-no'))
@@ -39,4 +39,4 @@ for i in number:
     lyric=driver.find_element(By.CLASS_NAME,"lyric")
     LYRIC.append(lyric.text)
 df=pd.DataFrame({"제목":title2,"가수":singer2,"가사":LYRIC})
-df.to_excel("멜론TOP50_가사8.xlsx",  encoding='utf-8')    
+df.to_excel("멜론TOP50_가사5.xlsx",  encoding='utf-8')    
