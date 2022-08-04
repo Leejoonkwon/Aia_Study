a559aeb2793da21ff4ac8cd0c969ee144b2af6c5
Leejoonkwon <107663853+Leejoonkwon@users.noreply.github.com>
2022-08-04 19:07:22 +0900

    0804

--- ml/m05_kfold_05_fetch_covtype.py
+++ ml/m05_kfold_05_fetch_covtype.py
@@ -11,41 +11,45 @@ datasets = fetch_covtype()
 x = datasets.data
 y = datasets['target']
 print(x.shape, y.shape) #(581012, 54) (581012,)
-from sklearn.svm import LinearSVC 
+from sklearn.model_selection import KFold,cross_val_score,cross_val_predict
+import warnings 
+warnings.filterwarnings('ignore')
+x_train, x_test ,y_train, y_test = train_test_split(
+          x, y, train_size=0.8,shuffle=True,random_state=100)
+n_split = 5
+kfold = KFold(n_splits=n_split, shuffle=True, random_state=66)
+from sklearn.preprocessing import MaxAbsScaler,RobustScaler 
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+# scaler = MinMaxScaler()
+scaler = StandardScaler()
+# scaler = MaxAbsScaler()
+# scaler = RobustScaler()
+scaler.fit(x_train) #여기까지는 스케일링 작업을 했다.
+scaler.transform(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.15,shuffle=True ,random_state=100)
-from tqdm import tqdm
-from sklearn.svm import LinearSVC,SVC
-from sklearn.linear_model import Perceptron ,LogisticRegression 
-#LogisticRegression은 유일하게 Regression이름이지만 분류 모델이다.
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier #공부하자 
-from sklearn.ensemble import RandomForestClassifier #공부하자 
-def models(model):
-    if model == 'knn':
-        mod = KNeighborsClassifier()
-    elif model == 'svc':
-        mod = SVC()
-    elif model == 'tree':
-        mod =  DecisionTreeClassifier()
-    elif model == 'forest':
-        mod =  RandomForestClassifier()
-    return mod
-model_list = ['knn', 'svc',  'tree', 'forest']
-empty_list = [] #empty list for progress bar in tqdm library
-for model in tqdm(model_list, desc = 'Models are training and predicting ... '):
-    empty_list.append(model) # fill empty_list to fill progress bar
-    #classifier
-    clf = models(model)
-    #Training
-    clf.fit(x_train, y_train) 
-    #Predict
-    result = clf.score(x_test,y_test)
-    pred = clf.predict(x_test) 
-    print('{}-{}'.format(model,result))
+print(x_test.shape)
 
+#2. 모델 구성
+from sklearn.utils import all_estimators
 
-# acc 스코어 : 0.8221842298512942
-########### ML 적용 후
-# results : 0.505496144666789
+allAlgorithms = all_estimators(type_filter='classifier')
+# allAlgorithms = all_estimators(type_filter='Regressor')
 
+# print('allAlgorithms :',allAlgorithms)
+print('모델의 갯수 :',len(allAlgorithms)) #모델의 갯수 : 41
+
+for (name,algorithms) in allAlgorithms:
+    try: # for문을 실행하는 와중에 예외 (error)가 발생하면 무시하고 진행 <예외처리>
+        model = algorithms()
+        model.fit(x_train,y_train)
+        
+        y_predict = model.predict(x_test)
+        acc = accuracy_score(y_test,y_predict)
+        scores = cross_val_score(model, x, y, cv=kfold)
+        # print('{}의 정확도 {}의 ',name,'의 정답률 :',acc)
+        print('{}의 정확도 :{} 검증 평균: {} '.format(name,round(acc,3),round(np.mean(scores),4)))
+       
+    except:
+        continue
