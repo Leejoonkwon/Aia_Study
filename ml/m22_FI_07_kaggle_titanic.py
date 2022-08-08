3f01726cc13ed892319b2907abc63f2ec5ce91d0
Leejoonkwon <107663853+Leejoonkwon@users.noreply.github.com>
2022-08-08 20:56:26 +0900

    0808

--- ml/m22_FI_07_kaggle_titanic.py
+++ ml/m22_FI_07_kaggle_titanic.py
@@ -184,7 +184,7 @@ models = [DecisionTreeClassifier(), RandomForestClassifier(),
           GradientBoostingClassifier(), XGBClassifier()]
 
 
-# 3. 컴파일, 훈
+# 3. 컴파일, 
 for model in models:
     model.fit(x_train, y_train)
     score = model.score(x_test, y_test)
