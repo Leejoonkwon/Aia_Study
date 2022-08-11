b96f4847510f14939ce7dc76a902aecf78fc6e44
Leejoonkwon <qnfk7895@naver.com>
2022-08-11 16:20:36 +0900

    0811

---
+++ ml/m36_dacon_travel.py
@@ -0,0 +1,26 @@
+from tensorflow.python.keras.models import Sequential,load_model
+from tensorflow.python.keras.layers import Dense,Dropout,Conv2D,Flatten,LSTM,Conv1D
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.callbacks import EarlyStopping,ModelCheckpoint
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+import matplotlib
+matplotlib.rcParams['font.family']='Malgun Gothic'
+matplotlib.rcParams['axes.unicode_minus']=False
+import pandas as pd
+from sklearn.svm import LinearSVC 
+from sklearn.svm import LinearSVR 
+import numpy as np 
+#1. 데이터
+path = 'D:\study_data\_data\_csv\dacon_travel/' # ".은 현재 폴더"
+train_set = pd.read_csv(path + 'train.csv',
+                        index_col=0)
+print(train_set)
+
+print(train_set.shape) #(1459, 10)
+
+test_set = pd.read_csv(path + 'test.csv', #예측에서 쓸거야!!
+                       index_col=0)
+submission = pd.read_csv(path + 'submission.csv',#예측에서 쓸거야!!
+                       index_col=0)
+                       
