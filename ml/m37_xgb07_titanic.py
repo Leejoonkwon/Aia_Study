aa2c93e1d715192d1cf3b4ceec8605427cfe8c7d
Leejoonkwon <qnfk7895@naver.com>
2022-08-12 21:17:54 +0900

    0811

--- ml/m37_xgb07_titanic.py
+++ ml/m37_xgb07_titanic.py
@@ -16,7 +16,7 @@ matplotlib.rcParams['font.family']='Malgun Gothic'
 matplotlib.rcParams['axes.unicode_minus']=False
     
 #1.데이터#############
-  ##
+  #####
 path = 'D:\study_data\_data\_csv\kaggle_titanic/' # ".은 현재 폴더"
 train_set = pd.read_csv(path + 'train.csv',
                         )
