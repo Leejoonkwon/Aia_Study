2769acb8b9ce8865e2759400e2f9f5eded187c48
Leejoonkwon <qnfk7895@naver.com>
2022-08-15 22:15:48 +0900

    0815

--- ml/m44_dacon_AI.py
+++ ml/m44_dacon_AI.py
@@ -8,6 +8,7 @@ import numpy as np
 from sklearn.model_selection import KFold, StratifiedKFold, train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.feature_selection import SelectFromModel
+from sklearn.decomposition import PCA
 
 #1. 데이터
 path = 'D:\study_data\_data\_csv\dacon_ai/' # ".은 현재 폴더"
@@ -19,8 +20,23 @@ test_set = pd.read_csv(path + 'test.csv', #예측에서 쓸거야!!
 
 train_x = train_set.filter(regex='X') # Input : X Featrue : 56
 train_y = train_set.filter(regex='Y') # Output : Y Feature : 14
+# print(test_set.isnull().sum())
+# print(train_y.isnull().sum())
+pca = PCA(n_components=17) # 차원 축소 (차원=컬럼,열,피처)
 
-print(train_x.info())
+x = pca.fit_transform(train_x)
+test_set = pca.transform(test_set) 
+# pca_EVR = pca.explained_variance_ratio_ # PCA로 압축 후에 새로 생성된 피쳐 임포턴스를 보여준다.
+# print(sum(pca_EVR)) #0.999998352533973
+# print(pca_EVR)
+
+# cumsum = np.cumsum(pca_EVR)
+# print(cumsum)
+# import matplotlib.pyplot as plt   
+# plt.plot(cumsum)
+# plt.grid()
+# plt.show()
+
 
 x_train,x_test,y_train,y_test=train_test_split(train_x,train_y,shuffle=True,random_state=1234,train_size=0.9)
 # print(train_x.shape,train_y.shape)  #(39607, 56) (39607, 14)     
@@ -37,23 +53,22 @@ from sklearn.metrics import r2_score
 
 n_splits = 5
 kfold = KFold(n_splits=n_splits,shuffle=True,random_state=123)
-# parameters = {'n_estimators':[100], # 디폴트 100/ 1~inf 무한대 
-# eta[기본값=0.3, 별칭: learning_rate]
-# gamma[기본값=0, 별칭: min_split_loss]
-# max_depth[기본값=6]
+# parameters = {'n_estimators':[100,200,300,400,500,1000], # 디폴트 100/ 1~inf 무한대 
+# eta[기본값=0.3, 별칭: learning_rate] learning_rate':[0.1,0.2,0.3,0.4,0.5,0.7,1]
+# max_depth': [1,2,3,4,5,6,7][기본값=6]
+# gamma[기본값=0, 별칭: min_split_loss] [0,0.1,0.3,0.5,0.7,0.8,0.9,1]
 # min_child_weight[기본값=1] 0~inf
-# subsample[기본값=1] 0~1
+# subsample[기본값=1][0,0.1,0.3,0.5,0.7,1] 0~1
 # colsample_bytree [0,0.1,0.2,0.3,0.5,0.7,1]    [기본값=1] 0~1
 # colsample_bylevel': [0,0.1,0.2,0.3,0.5,0.7,1] [기본값=1] 0~1
 # 'colsample_bynode': [0,0.1,0.2,0.3,0.5,0.7,1] [기본값=1] 0~1
 # 'reg_alpha' : [0,0.1 ,0.01, 0.001, 1 ,2 ,10]  [기본값=0] 0~inf /L1 절댓값 가중치 규제 
 # 'reg_lambda' : [0,0.1 ,0.01, 0.001, 1 ,2 ,10]  [기본값=1] 0~inf /L2 절댓값 가중치 규제 
-# max_delta_step[기본값=0]
 
-parameters = {'n_estimators':[100],
+parameters = {'n_estimators':[100,200],
               'learning_rate':[0.1,0.2,0.3],
-            #   'max_depth': [3],
-            #   'gamma' : [1],
+              'max_depth': [5,6,7],
+              'gamma' : [0,0.1,0.3],
             #   'min_child_weight' : [1],
             #   'subsample' : [1],
             #   'colsample_bytree' : [0.5],
