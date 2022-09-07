1ac0e6df57fdf0b08516e21560db719a6a468890
Leejoonkwon <qnfk7895@naver.com>
2022-09-07 15:23:09 +0900

    0907

--- ml/m57_dacon_grow.py
+++ ml/m57_dacon_grow.py
@@ -68,7 +68,7 @@ print(label_data)   # 1440
 print(train_data.shape, label_data.shape)   # (1607, 1440, 37) (1607,)
 print(val_data.shape, val_target.shape)   # (206, 1440, 37) (206,)
                    
-             
+                 
 x_train,x_test,y_train,y_test = train_test_split(train_data,label_data,train_size=0.90,shuffle=False)
 
 #2. 모델 구성
